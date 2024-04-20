ec19d71f2f5d7468bce498a94af6222e48f94b32
saintslisi <santilisi88@gmail.com>
2024-04-20 19:12:17 +0200

    new

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const stripe = require('stripe')('YOUR_STRIPE_SECRET_KEY');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/submit-payment', async (req, res) => {
+    const { cardNumber, expiry, cvc, cardholderName } = req.body;
+
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 1000, // Importo in centesimi (es. $10.00)
+            currency: 'usd',
+            payment_method_types: ['card'],
+            capture_method: 'automatic',
+            payment_method_data: {
+                type: 'card',
+                card: {
+                    number: cardNumber,
+                    exp_month: expiry.split('/')[0],
+                    exp_year: expiry.split('/')[1],
+                    cvc: cvc
+                }
+            }
+        });
+
+        res.json({ success: true, paymentIntent });
+    } catch (error) {
+        console.error('Errore durante il pagamento:', error);
+        res.status(500).json({ success: false, error: 'Errore durante il pagamento' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
